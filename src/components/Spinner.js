ee6b77d9eae14fb4061b81f23e76d2dae12139a1
Rohit <rajputrohit2kk@gmail.com>
2022-07-15 00:49:08 +0530

    make it better

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import loading from './loading.gif'
-const spinner = () => {
- 
+
+const Spinner = ()=> {
         return (
             <div className="text-center">
-                 <img  className='my-3' src={loading} alt="loading" />
-           </div>
+                <img className="my-3" src={loading} alt="loading" />
+            </div>
         )
-    
 }
-export default spinner
+
+export default Spinner
